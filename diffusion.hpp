07a5cc133695e661d426ed076488228d5958b47c
Nicole Lunedei <lunemonday@gmail.com>
2024-03-18 23:30:58 +0100

    Setting Working Place

---
+++ diffusion.hpp
@@ -0,0 +1,15 @@
+ //#ifndef DIFFUSION_HPP
+ //#define DIFFUSION_HPP
+ struct People{
+
+ };
+ struct Parameters{
+
+ };
+
+ class Diffusion{
+    //costruttore default
+    //costruttore per controlli e inizializzazione 
+
+ };
+ //#endif 
