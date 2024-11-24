1ea88b7aa0415a494a3123b1f042f455d5ad9d9c
nicoleLunedei <lunemonday@gmail.com>
2024-11-24 17:49:15 +0100

    Building of matrix and pandemic

---
+++ build/CMakeFiles/sihd_mat_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sihd_mat_test.dir/matrix_test.cpp.o"
+  "CMakeFiles/sihd_mat_test.dir/matrix_test.cpp.o.d"
+  "sihd_mat_test"
+  "sihd_mat_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sihd_mat_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sihd_pan_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sihd_pan_test.dir/pandemic.cpp.o"
+  "CMakeFiles/sihd_pan_test.dir/pandemic.cpp.o.d"
+  "CMakeFiles/sihd_pan_test.dir/pandemic_test.cpp.o"
+  "CMakeFiles/sihd_pan_test.dir/pandemic_test.cpp.o.d"
+  "sihd_pan_test"
+  "sihd_pan_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sihd_pan_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
